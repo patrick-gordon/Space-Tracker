e5d82e27205ba68c2832d78a0a40a07859d98af8
patrick-gordon <patrickgordon42@yahoo.com>
2021-02-08 14:29:52 -0600

    converted navbar to css from inline and fixed for mobile w/ media queries

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,54 +12,46 @@ import {
 } from "reactstrap";
 import { faAlignJustify } from "@fortawesome/free-solid-svg-icons";
 import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import "./NavBar.css";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleNavbar = () => setIsOpen(!isOpen);
 
   return (
-        <Container>
-          <Navbar style={{ backgroundColor: "#00000", maxHeight: "10rem" }}>
-          <FontAwesomeIcon icon={faRocket} size="2x" className='mr-3'></FontAwesomeIcon>
-          <NavbarBrand
-            href="/"
-            className="mr-auto"
-            style={{ color: "#FFFF", fontSize: "36px" }}
-          >
-            Space Tracker
-          </NavbarBrand>
-          <NavbarToggler
-            onClick={toggleNavbar}
-            className="mr-2"
-            style={{ border: "1px solid black" }}
-          >
-            {" "}
-            <FontAwesomeIcon icon={faAlignJustify} color={"#FFF"} size="1x" />
-          </NavbarToggler>
-          <Collapse isOpen={isOpen} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink
-                  href="/nasa"
-                  style={{ color: "#FFF", fontSize: "22px" }}
-                  className='ml-5'
-                >
-                  Nasa
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="/spacex"
-                  style={{ color: "#FFF", fontSize: "22px" }}
-                  className='ml-5'
-                >
-                  SpaceX
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
+    <Container>
+      <Navbar className="main-navbar">
+        <FontAwesomeIcon
+          icon={faRocket}
+          size="2x"
+          className="mr-3"
+        ></FontAwesomeIcon>
+        <NavbarBrand className="main-navbar-brand" href="/">
+          Space Tracker
+        </NavbarBrand>
+        <NavbarToggler
+          onClick={toggleNavbar}
+          className="mr-2"
+          style={{ border: "1px solid white" }}
+        >
+          {" "}
+          <FontAwesomeIcon icon={faAlignJustify} color={"#FFF"} size="1x" />
+        </NavbarToggler>
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink className="main-navbar-sublink-nasa" href="/nasa">
+                Nasa
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="main-navbar-sublink-spacex" href="/spacex">
+                SpaceX
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
-        </Container>
-    
+    </Container>
   );
 }
--- src/components/nasa/NasaHomePage.js
+++ src/components/nasa/NasaHomePage.js
@@ -3,38 +3,17 @@ import { Card, CardTitle, Container } from "reactstrap";
 import { useHistory } from "react-router-dom";
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./NasaHomePage.css";
 
 export default function NasaHomePage() {
   const history = useHistory();
 
   return (
-    <div
-      style={{
-        // border: "1px solid white",
-        display: "flex",
-        marginTop: '2rem'
-      }}
-    >
-      <Container
-        style={{
-          // border: "1px solid white",
-          // width: "1216px",
-          height: "100%",
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-around",
-        }}
-      >
+    <div className="nasa-homepage-wrapper">
+      <Container className="nasa-homepage-container">
         <Card
           inverse
-          style={{
-            backgroundColor: "#343435",
-            width: "20.5rem",
-            height: "15rem",
-            textAlign: "center",
-            alignItems: "center",
-            marginTop: '2rem'
-          }}
+          className="nasa-homepage-card"
           onClick={() => history.push("/apod")}
         >
           <CardTitle tag="h5" style={{ marginTop: "1rem" }}>
@@ -46,35 +25,16 @@ export default function NasaHomePage() {
             style={{ marginTop: "2rem" }}
           />
         </Card>
-        {/* <Card
-          inverse
-          style={{
-            backgroundColor: "#726D7A",
-            width: "365px",
-            height: "262px",
-            textAlign: "center",
-            alignItems: 'center'
-          }}
-        >
-          <CardTitle tag="h5">APOD</CardTitle>
-          <FontAwesomeIcon icon={faArrowAltCircleRight} size='5x' onClick={() => history.push("/apod")} />
-        </Card> */}
       </Container>
 
-      <Container
-        style={{
-          width: "400px",
-          // border: "1px solid white",
-          marginTop: '2rem'
-        }}
-      >
+      <Container className="nasa-twitter-homepage-container">
         <a
           class="twitter-timeline"
           href="https://twitter.com/nasa?ref_src=twsrc%5Etfw"
           data-height="600"
-          data-chrome='transparent'
-          data-border-color='#FFFF'
-          data-theme='dark'
+          data-chrome="transparent"
+          data-border-color="#FFFF"
+          data-theme="dark"
         >
           Tweets by Nasa
         </a>
